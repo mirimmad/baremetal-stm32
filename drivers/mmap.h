468635f4bcf6cc4c750e306e6ca6d98a21c07475
Immad Mir <mirimmad17@gmail.com>
2024-05-22 22:07:07 +0530

    drivers: add SCS memory macros

--- drivers/mmap.h
+++ drivers/mmap.h
@@ -3,6 +3,11 @@
 
 #include <stdint.h>
 
+#define SCS_BASE 0xE000E000U
+#define SysTick_BASE (SCS_BASE + 0x0010)
+#define NVIC_BASE (SCS_BASE + 0x0100)
+#define SCB_BASE (SCS_BASE + 0x0D00)
+
 #define RCC_BASE 0x40023800
 
 #define GPIOA_BASE 0x40020000
