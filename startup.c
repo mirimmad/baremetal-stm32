c4041e9d0763831a04d279670c238ba1578ea4a5
Immad Mir <mirimmad17@gmail.com>
2024-05-25 21:04:06 +0530

    startup: Add more interrupt handlers and weak denfinitions

--- startup.c
+++ startup.c
@@ -15,6 +15,8 @@ void reset_handler(void);
 
 void defaultHandler(void);
 void SysTickHandler(void) __attribute__((weak));
+void EXTI9_5_Handler(void) __attribute__((weak));
+void EXT15_10_Handler(void) __attribute__((weak));
 
 __attribute__((used, section(".vector_table"))) void (*const vectors[])(void) = {
     &_estack,
@@ -33,6 +35,53 @@ __attribute__((used, section(".vector_table"))) void (*const vectors[])(void) = 
     defaultHandler, // reserved
     defaultHandler, // PendSV
     SysTickHandler, // System tick timer (SysTick)
+
+    defaultHandler, // Window WatchDog
+    defaultHandler, // PVD through EXTI Line detection
+    defaultHandler, // Tamper and TimeStamps through the EXTI line
+    defaultHandler, // RTC Wakeup through the EXTI line
+    defaultHandler, // FLASH
+    defaultHandler, // RCC
+
+    defaultHandler, // EXTI Line0
+    defaultHandler, // EXTI Line1
+    defaultHandler, // EXTI Line2
+    defaultHandler, // EXTI Line3
+    defaultHandler, // EXTI Line4
+
+    defaultHandler, // DMA1 Stream 0
+    defaultHandler, // DMA1 Stream 1
+    defaultHandler, // DMA1 Stream 2
+    defaultHandler, // DMA1 Stream 3
+    defaultHandler, // DMA1 Stream 4
+    defaultHandler, // DMA1 Stream 5
+    defaultHandler, // DMA1 Stream 6
+    defaultHandler, // ADC1, ADC2 and ADC3s
+
+    defaultHandler, // reserved
+    defaultHandler, // reserved
+    defaultHandler, // reserved
+    defaultHandler, // reserved
+
+    EXTI9_5_Handler, // EXT1 Lines[9:5]
+    defaultHandler,
+    defaultHandler,
+    defaultHandler,
+    defaultHandler,
+    defaultHandler,
+    defaultHandler,
+    defaultHandler,
+    defaultHandler,
+    defaultHandler,
+    defaultHandler,
+    defaultHandler,
+    defaultHandler,
+    defaultHandler,
+    defaultHandler,
+    defaultHandler,
+    defaultHandler,
+    EXT15_10_Handler, // EXT1 Lines[15:10]
+
 };
 
 __attribute__((used, section(".reset_handler")))
@@ -59,9 +108,18 @@ void reset_handler(void) {
 void defaultHandler(void) {
     // AIRCR
     // cause system reset
-    *(uint32_t*)0xE000ED0C |= (1 << 2);
+    //*(uint32_t*)0xE000ED0C |= (1 << 2);
+    __asm__("bkpt 2");
 }
 
 void SysTickHandler(void) {
     __asm__("bkpt 1");
+}
+
+void EXTI9_5_Handler(void) {
+    __asm__("bkpt 3");
+}
+
+void EXT15_10_Handler(void) {
+    __asm__("bkpt 4");
 }
